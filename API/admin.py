52d5001168045e81640791e369a7b010f7dad6a7
earl cameron <mr.e.cmaeron@gmail.com>
2019-01-05 16:35:00 -0500

    started pasword recovery

--- API/admin.py
+++ API/admin.py
@@ -6,35 +6,42 @@ from classes.admin import Admin_test
 
 admins = Blueprint('admins', __name__,)
 
-
 @admins.route('admin', methods=['GET', 'POST', 'DELETE', 'OPTION', 'PUT'])
 def index():
 
+    # http header with auth token, quering database if toekn exist
     token = request.headers['Authorization']
     token_obj = Token.query.filter_by(token=token).first()
 
-    # checking to see if the token exists
+    # debug message checking to see if the token exists
     print('token exists --> ', not token_obj is None)
 
     if request.method == 'POST':
+
+        # if toekn is not empty
         if token_obj != None:
 
+            # list to store all admins
             data = []
+
+            # looping through all admins
             for admin in Admin_test.query.all():
                 data.append({"name": admin.name, "email": admin.email, "role":admin.role.value})
 
+            # return a json spec list of admins
             response = jsonify(data)
 
         elif token_obj == None:
 
+            # if the token doesn't exist then user is not authorized
             response = Response(status=401)
             response.data = '{"message":"Not authorized"}'
 
     else:
 
+        # if HTTP method isn't POST then method not allowed
         response = Response(status=405)
         response.data = '{"message":"Method not allowed"}'
 
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-    # return json.dumps(users)
--- routes/recover.py
+++ routes/recover.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, session
 from classes.admin import Admin_test
+from config.config import db
+from utils.security import *
 
 recover = Blueprint('recover', __name__)
 
@@ -21,5 +23,32 @@ def require_login():
 def index():
     title = 'login'
     if request.method == 'POST':
-        print('POST')
+
+        # form data
+        email = request.form['email']
+        question = request.form['question']
+        answer = request.form['answer']
+
+        # query to see if user exists
+        query = Admin_test.query.filter_by(email=email).first()
+
+        # if the user exists then do the following
+        if not query is None:
+            
+            # makes string of the email, question number and answer
+            user_password =email + question + answer
+
+            # compare the old hash against a newly hashed userpassword
+            if verify_password(query.recovery, user_password):
+
+                # returns the template that will allows users to reset password
+                return render_template('recover.html', title=title)
+
+        # if the user deson't exist
+        else:
+            # prompt the user that the email provided doesnt have a user
+            error = "User Doesn't exist"
+            return render_template('recover.html', title=title, error=error)
+
+    # returns template on GET
     return render_template('recover.html', title=title)
